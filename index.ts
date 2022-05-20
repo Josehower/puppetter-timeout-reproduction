41f1e96b620e6d960e5ef81fb2d7adf21b2d7eb3
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2022-05-20 12:42:19 +0200

    Clean the code

--- index.ts
+++ index.ts
@@ -6,16 +6,7 @@ function wait(ms) {
 
 export async function puppeteerWorkflow(urlToTest: string) {
   const browser = await puppeteer.launch({
-    ...(process.platform === 'linux'
-      ? {
-          executablePath: '/usr/bin/google-chrome-stable',
-        }
-      : {
-          // If we're not on Linux, then maybe we're in development,
-          // where we don't want a headless browser (we want to see what
-          // is going on)
-          headless: false,
-        }),
+    executablePath: '/usr/bin/google-chrome-stable',
   });
   const page = await browser.newPage();
 
@@ -25,9 +16,6 @@ export async function puppeteerWorkflow(urlToTest: string) {
   const [element] = await page.$x(`//label[contains(text(), 'Top text')]`);
   element.type('lol');
   img.click();
-  // await wait(5000);
-  await page.waitForNetworkIdle();
-  await page.waitForNetworkIdle();
   await page.waitForNetworkIdle();
   await page.close();
   await browser.close();
