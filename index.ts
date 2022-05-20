98dfe5d3f1f7e1f7ccd01d73818403ad13ebc38c
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2022-05-20 13:53:21 +0200

    Add extra types to do repo more prompt to error

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import puppeteer from 'puppeteer';
+import puppeteer, { ElementHandle } from 'puppeteer';
 
 export async function puppeteerWorkflow(urlToTest: string) {
   const browser = await puppeteer.launch({
@@ -7,50 +7,51 @@ export async function puppeteerWorkflow(urlToTest: string) {
   const page = await browser.newPage();
   await page.goto(urlToTest);
 
+  const [element] = (await page.$x(
+    `//label[contains(text(), 'Top text')]`,
+  )) as [ElementHandle<HTMLInputElement>];
+
+  await element.type('lol');
+
   console.log('network test start...');
   await page.waitForNetworkIdle();
-  console.log('network check 1 pass');
+  await console.log('network check 1 pass');
+
+  await element.type('lol');
   await page.waitForNetworkIdle();
   console.log('network check 2 pass');
+
+  await element.type('lol');
   await page.waitForNetworkIdle();
   console.log('network check 3 pass');
+
+  await element.type('lol');
   await page.waitForNetworkIdle();
   console.log('network check 4 pass');
+
+  await element.type('lol');
   await page.waitForNetworkIdle();
   console.log('network check 5 pass');
 
+  await element.type('lol');
   await page.waitForNetworkIdle();
   console.log('network check 6 pass');
+
+  await element.type('lol');
   await page.waitForNetworkIdle();
   console.log('network check 7 pass');
+
+  await element.type('lol');
   await page.waitForNetworkIdle();
   console.log('network check 8 pass');
+
+  await element.type('lol');
   await page.waitForNetworkIdle();
   console.log('network check 9 pass');
+
+  await element.type('lol');
   await page.waitForNetworkIdle();
   console.log('network check 10 pass');
-
-  await page.waitForNetworkIdle();
-  console.log('network check 11 pass');
-  await page.waitForNetworkIdle();
-  console.log('network check 12 pass');
-  await page.waitForNetworkIdle();
-  console.log('network check 13 pass');
-  await page.waitForNetworkIdle();
-  console.log('network check 14 pass');
-  await page.waitForNetworkIdle();
-  console.log('network check 15 pass');
-
-  await page.waitForNetworkIdle();
-  console.log('network check 16 pass');
-  await page.waitForNetworkIdle();
-  console.log('network check 17 pass');
-  await page.waitForNetworkIdle();
-  console.log('network check 18 pass');
-  await page.waitForNetworkIdle();
-  console.log('network check 19 pass');
-  await page.waitForNetworkIdle();
-  console.log('network check 20 pass');
 
   await page.close();
   await browser.close();
