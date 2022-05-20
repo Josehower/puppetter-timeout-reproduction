ccf2af0b9fd8a6e1a38ef99c1305593104a40423
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2022-05-20 13:11:10 +0200

    Add checks for each witForNetworkIdle

--- index.ts
+++ index.ts
@@ -1,48 +1,55 @@
 import puppeteer from 'puppeteer';
 
-function wait(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 export async function puppeteerWorkflow(urlToTest: string) {
   const browser = await puppeteer.launch({
     executablePath: '/usr/bin/google-chrome-stable',
   });
   const page = await browser.newPage();
 
-  // await page.goto(urlToTest);
-  // const img = await page.$('[data-test-id="meme-image"]');
-  // img.click();
-  // const [element] = await page.$x(`//label[contains(text(), 'Top text')]`);
-  // element.type('lol');
-  // img.click();
+  await page.waitForNetworkIdle();
+  console.log('network check 1 pass');
+  await page.waitForNetworkIdle();
+  console.log('network check 2 pass');
+  await page.waitForNetworkIdle();
+  console.log('network check 3 pass');
+  await page.waitForNetworkIdle();
+  console.log('network check 4 pass');
+  await page.waitForNetworkIdle();
+  console.log('network check 5 pass');
 
   await page.waitForNetworkIdle();
+  console.log('network check 6 pass');
   await page.waitForNetworkIdle();
+  console.log('network check 7 pass');
   await page.waitForNetworkIdle();
+  console.log('network check 8 pass');
   await page.waitForNetworkIdle();
+  console.log('network check 9 pass');
   await page.waitForNetworkIdle();
+  console.log('network check 10 pass');
 
   await page.waitForNetworkIdle();
+  console.log('network check 11 pass');
   await page.waitForNetworkIdle();
+  console.log('network check 12 pass');
   await page.waitForNetworkIdle();
+  console.log('network check 13 pass');
   await page.waitForNetworkIdle();
+  console.log('network check 14 pass');
   await page.waitForNetworkIdle();
+  console.log('network check 15 pass');
 
   await page.waitForNetworkIdle();
+  console.log('network check 16 pass');
   await page.waitForNetworkIdle();
+  console.log('network check 17 pass');
   await page.waitForNetworkIdle();
+  console.log('network check 18 pass');
   await page.waitForNetworkIdle();
+  console.log('network check 19 pass');
   await page.waitForNetworkIdle();
-
-  await page.waitForNetworkIdle();
-  await page.waitForNetworkIdle();
-  await page.waitForNetworkIdle();
-  await page.waitForNetworkIdle();
-  await page.waitForNetworkIdle();
+  console.log('network check 20 pass');
 
   await page.close();
   await browser.close();
 }
-
-// puppeteerWorkflow('https://drone-break-test.netlify.app/');
